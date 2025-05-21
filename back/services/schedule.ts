ca09899be77ed18db7799f7b0803328127c4b222
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-05-21 15:57:53 +0800

    Update schedule.ts

--- back/services/schedule.ts
+++ back/services/schedule.ts
@@ -186,7 +186,7 @@ export default class ScheduleService {
     const job = new LongIntervalJob(
       { runImmediately: false, ...schedule },
       task,
-      _id,
+      { id: _id },
     );
 
     this.intervalSchedule.addIntervalJob(job);
