f03cf73345af85360a38bba9268fe47b7c99a6cb
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-05-22 07:24:34 +0800

    修复删除日志提示

--- src/pages/log/index.tsx
+++ src/pages/log/index.tsx
@@ -117,7 +117,7 @@ const Log = () => {
           <Text style={{ wordBreak: 'break-all' }} type="warning">
             {select}
           </Text>
-          {intl.get('文件')}{currentNode.type === 'directory' ? intl.get('夹下所以日志') : ''}
+          {intl.get('文件')}{currentNode.type === 'directory' ? intl.get('夹下所有日志') : ''}
           {intl.get('，删除后不可恢复')}
         </>
       ),
